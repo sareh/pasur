169b11e219f3f08bea2e3bf06fa54674e912dc06
sareh <sareh.heidari@gmail.com>
2015-10-10 06:41:14 +0100

    Initialisation of app.js

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(function(){
+  console.log("Ready to play Pasur!");
+
+  var playerHand = [];
+  var computerHand = [];
+  var playerStagingArea = [];
+  var computerStagingArea = [];
+  var pool = [];
+
+  var playerStash = [];
+  var computerStash = [];
+  var playerPoints = 0;
+  var computerPoints = 0;
+  var unplayedCards = [];
+  var message = "";
+
+  var singleCard = {suit: "club",
+             face: 1,
+             points: 1,
+             backimg: "./images/back.png"
+             faceimg: "./images/1_1.png"
+             };
+
+
+})
